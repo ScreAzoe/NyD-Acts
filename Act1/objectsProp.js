0a8889a21415cb1b198a630c0e289075d3d60ce1
screazoe <castillorealberto@live.com>
2021-10-07 18:46:50 -0500

    Version completa de Script

--- Act1/objectsProp.js
+++ Act1/objectsProp.js
@@ -2,12 +2,31 @@
 
 function enfocarImagen(id_imagen){
     var imagen= document.querySelector(id_imagen);
-    imagen.width= "400";
-    imagen.height= "400";
+    imagen.width= "300";
+    imagen.height= "300";
 }
 
 function desenfocarImagen(id_imagen){
     var imagen= document.querySelector(id_imagen);
-    imagen.width= "300";
-    imagen.height= "300";
+    imagen.width= "200";
+    imagen.height= "200";
+}
+
+function seleccionarItem(nombre, precio){
+    var formulario= document.querySelector('#formularioPedido');
+    formulario.producto.value= nombre;
+    formulario.precio.value=  precio;
+    formulario.cantidad.value= 1;
+    formulario.total.value= precio;
+    formulario.boton_pagar.disabled=false;
+}
+
+function calcularTotal(){
+    let formulario= document.querySelector('#formularioPedido');
+    formulario.total.value= formulario.cantidad.value*formulario.precio.value;
+}
+
+function limpiar(){
+    let formulario= document.querySelector('#formularioPedido');
+    formulario.boton_pagar.disabled= true;
 }
